46fbae467b0625fe28b9bd95db3332aef66f53c1
MatusalaErmiyas <matusala.dev@gmail.com>
2024-09-03 22:09:53 +0300

    Add logger

--- services/database.js
+++ services/database.js
@@ -1,8 +1,10 @@
 const { default: mongoose } = require("mongoose");
+const { logger } = require("./logger");
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect("'mongodb://127.0.0.1:27017/musically");
+    await mongoose.connect("mongodb://127.0.0.1:27017/musically");
+    logger.info("Connected to database successfully");
   } catch (ex) {
     process.exit(1);
   }
