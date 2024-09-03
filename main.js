db4238b004007cff9c9ece7062d344be1a4da43c
MatusalaErmiyas <matusala.dev@gmail.com>
2024-09-03 22:12:50 +0300

    Implement main functionality

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 const express = require("express");
-const { songsRouter } = require("./songs/songs_route,js");
 const { logger } = require("./services/logger");
+
+const { connectToDatabase } = require("./services/database");
+
+const { songsRouter } = require("./songs/songs_route");
+const { albumsRouter } = require("./albums/albums_route.js");
+const { artistsRouter } = require("./artists/artists_route.js");
+const { genresRouter } = require("./genres/genres_route.js");
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use("/songs", songsRouter);
+async function main() {
+  await connectToDatabase();
 
-app.listen(PORT, () => logger.info(`Server listening on port ${PORT}`));
+  app.use(express.json());
+  app.use(require("morgan")("tiny"));
+
+  app.use("/songs", songsRouter);
+  app.use("/albums", albumsRouter);
+  app.use("/artists", artistsRouter);
+  app.use("/genres", genresRouter);
+
+  app.listen(PORT, () => logger.info(`Server listening on port ${PORT}`));
+}
+
+main();
