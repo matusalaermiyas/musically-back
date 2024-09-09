963f2c88e8dbcd22d26fb337979d2e7e0c8e1dd6
MatusalaErmiyas <matusala.dev@gmail.com>
2024-09-09 09:34:03 +0300

    Add env database url

--- services/database.js
+++ services/database.js
@@ -1,10 +1,10 @@
 const { default: mongoose } = require("mongoose");
-const { logger } = require("./logger");
+const { appLoger } = require("./logger");
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/musically");
-    logger.info("Connected to database successfully");
+    await mongoose.connect(process.env.DATABASE_URL);
+    appLoger.info("Connected to database successfully");
   } catch (ex) {
     process.exit(1);
   }
