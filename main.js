c1b74d80ea9bcb9ba5c61f326bdf24a90ba05b2d
MatusalaErmiyas <matusala.dev@gmail.com>
2024-09-10 18:13:59 +0300

    Change the port

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const { artistsRouter } = require("./artists/artists_route.js");
 const { genresRouter } = require("./genres/genres_route.js");
 const { statisticsRoute } = require("./statistics/statistics_route.js");
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
